0c527fbf58d74d2be9adee0ca32b18a1b53e86d3
Alberto Riolly <albert.r.cahyantara@gmail.com>
2020-09-01 10:10:49 +0700

    CRUD for Post

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import mikroOrmConfig from "./mikro-orm.config";
-import { Post } from "./entities/Post";
+import express from 'express';
+import { ApolloServer } from "apollo-server-express";
+import { PostResolver } from "./resolvers/post";
+import { buildSchema } from "type-graphql";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
-  
   await orm.getMigrator().up();
-  // const postFirst = orm.em.create(Post, { title: "This is my first post" });
-  // orm.em.persistAndFlush(postFirst);
-
-  const posts = await orm.em.find(Post, {});
-  console.log(posts)
+  
+  const app = express();
+  
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  })
+  apolloServer.applyMiddleware({ app });
+  
+  app.listen(2000, () => console.log("App is running at localhost: 2000"));
 }
 
 main().catch((err) => console.error(err));
